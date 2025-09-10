b2e9dccfc9aec786290ad6cc5e370e7594048199
Калинин Игорь <lapis-lines@list.ru>
2025-09-10 17:31:50 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -2,10 +2,28 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/web")
 def start():
     return """<!doctype html>
         <html>
            <body>
                <hl>web-сервер на flask</h1>
+               <p><a href="/author">author</a></p>
            </body>
+        </html>"""
+
+@app.route("/author")
+def author():
+    name = "Калинин Игорь Евгеньевич"
+    group = "ФБИ-32"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/web">web</a>
+            </body>
         </html>"""
