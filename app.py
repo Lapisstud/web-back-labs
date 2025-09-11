ee1124cf373b355f857d7bd0c265adece4e34ba3
Калинин Игорь <lapis-lines@list.ru>
2025-09-11 14:36:21 +0700

    Меню первой лабораторной

--- app.py
+++ app.py
@@ -34,6 +34,27 @@ def main():
     </body>
 </html>'''
 
+@app.route("/lab1")
+def lab1():
+    Stylesheet = url_for("static", filename="lab1.css")
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="''' + Stylesheet + '''">
+        <title> Лабораторная 1 </title>
+    </head>
+    <body>
+        <div class="container">
+            <p>Flask — фреймворк для создания веб-приложений на языке программирования Python, использующий набор инструментов 
+            Werkzeug, а также шаблонизатор Jinja2. Относится к категории так называемых микрофреймворков — минималистичных каркасов 
+            веб-приложений, сознательно предоставляющих лишь самые базовые возможности.</p>
+
+            <a href="/">Вернуться на главную</a> 
+        </div>
+    </body>
+</html>'''
+
 @app.route("/lab1/web")
 def web():
     return '''<!doctype html>
