06dffc592436d43a965786c9f0f1fa45b92bfc32
Калинин Игорь <lapis-lines@list.ru>
2025-10-08 16:11:22 +0700

    Имя студента

--- app.py
+++ app.py
@@ -370,4 +370,5 @@ def add_flower(name):
 
 @app.route('/lab2/example/')
 def example():
-    return render_template('example.html')
+    name = 'Игорь Калинин'
+    return render_template('example.html', name=name)
